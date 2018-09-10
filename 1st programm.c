e8c54d8d9c79e9fdc6ee8976661bd829e2a6d243
Parizat Binte Kabir <43074604+pbkabir@users.noreply.github.com>
2018-09-10 14:04:35 +0600

    Add files via upload

---
+++ 1st programm.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int a,b,sum;
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("sum is %d",sum);
+
+}
+int add(a,b)
+{
+    return(a+b);
+}
---
+++ Epx_2_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+float base,height,area;
+base=20;
+height=10;
+area=0.5*base*height;
+
+printf("The area of the triangle is %f",area);
+}
