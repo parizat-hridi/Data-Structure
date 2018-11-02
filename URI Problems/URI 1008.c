03954a70fb24435c8b3aceef3142a2efa39b98c8
Parizat Binte Kabir <43074604+pbkabir@users.noreply.github.com>
2018-11-03 00:17:15 +0600

    Code

---
+++ URI Problems/URI 1008.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int n,h;
+float p;
+double ts;;
+scanf("%d %d %f",&n,&h,&p);
+ts = h*p;
+
+printf("NUMBER = %d\n",n);
+printf("SALARY = U$ %0.2lf\n",ts);
+return 0;
+}
